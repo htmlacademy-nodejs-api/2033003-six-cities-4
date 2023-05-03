ee2f75b2872af6027487ac0a0a1e0c60190ffb96
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-05-03 21:08:31 +0300

    feat: Add CLIApplication for parsing and registering commands
    Поменял расположение файлов

---
+++ src/app/cli.ts
@@ -0,0 +1,33 @@
+import { CliCommandInterface } from '../core/cli-command.interface';
+
+type ParsedCommand = {
+  [key: string]: string[]
+}
+
+export default class CLIApplication {
+  private commands: {[propertyName: string]: CliCommandInterface} = {};
+
+  private parseCommand(cliArguments: string[]): ParsedCommand {
+    const parsedCommand: ParsedCommand = {};
+    let command = '';
+
+    return cliArguments.reduce((acc, item) => {
+      if (item.startsWith('--')) {
+        acc[item] = [];
+        command = item;
+      } else if (command && item) {
+        acc[command].push(item);
+      }
+
+      return acc;
+    }, parsedCommand);
+  }
+
+  public registerCommands(commandList: CliCommandInterface[]): void {
+    commandList.reduce((acc, command) => {
+      const cliCommand = command;
+      acc[cliCommand.name] = cliCommand;
+      return acc;
+    }, this.commands);
+  }
+}
