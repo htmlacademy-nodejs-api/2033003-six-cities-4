1be2c90a620b4f2d986135859e9da3b36456f4d7
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-05-03 23:11:19 +0300

    fix: linter errors in TSVFileReader

--- src/core/file-reader/tsv-file-reader.ts
+++ src/core/file-reader/tsv-file-reader.ts
@@ -22,44 +22,44 @@ export default class TSVFileReader implements FileReaderInterface {
     }
 
     return this.rawData
-    .split('\n')
-    .filter((row) => row.trim() !== '')
-    .map((line) => line.split('\t'))
-    .map(([title, description, publicationDate, city, previewImage, images, isPremium, isFavorite, rating, type, rooms, guests, price, amenities, author, email, password, userType, commentsCount]) => {
-      const parsedAmenities = amenities.split(';').filter((amenity) => amenity !== '');
-      const parsedImages = images.split(';');
-      const cityCoordinates: CityCoordinates = cities[city];
-      const { latitude, longitude } = cityCoordinates;
+      .split('\n')
+      .filter((row) => row.trim() !== '')
+      .map((line) => line.split('\t'))
+      .map(([title, description, publicationDate, city, previewImage, images, isPremium, isFavorite, rating, type, rooms, guests, price, amenities, author, email, password, userType, commentsCount]) => {
+        const parsedAmenities = amenities.split(';').filter((amenity) => amenity !== '');
+        const parsedImages = images.split(';');
+        const cityCoordinates: CityCoordinates = cities[city];
+        const { latitude, longitude } = cityCoordinates;
 
-      const rentalOffer: RentalOffer = {
-        title,
-        description,
-        publicationDate: new Date(publicationDate),
-        city,
-        previewImage,
-        images: parsedImages,
-        isPremium: Boolean(isPremium),
-        isFavorite: Boolean(isFavorite),
-        rating: parseFloat(rating),
-        type: type as RentalType,
-        rooms: parseInt(rooms, 10),
-        guests: parseInt(guests, 10),
-        price: parseFloat(price),
-        amenities: parsedAmenities as Amenities[],
-        author: {
-          name: author,
-          email,
-          password,
-          userType: userType as UserType
-        },
-        commentsCount: parseInt(commentsCount, 10),
-        coordinates: {
-          latitude,
-          longitude
-        }
-      };
+        const rentalOffer: RentalOffer = {
+          title,
+          description,
+          publicationDate: new Date(publicationDate),
+          city,
+          previewImage,
+          images: parsedImages,
+          isPremium: Boolean(isPremium),
+          isFavorite: Boolean(isFavorite),
+          rating: parseFloat(rating),
+          type: type as RentalType,
+          rooms: parseInt(rooms, 10),
+          guests: parseInt(guests, 10),
+          price: parseFloat(price),
+          amenities: parsedAmenities as Amenities[],
+          author: {
+            name: author,
+            email,
+            password,
+            userType: userType as UserType
+          },
+          commentsCount: parseInt(commentsCount, 10),
+          coordinates: {
+            latitude,
+            longitude
+          }
+        };
 
-      return rentalOffer;
-    });
+        return rentalOffer;
+      });
   }
 }
