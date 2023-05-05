1194775c21847f85e4ef892ca7be7fc9f967d070
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-05-05 18:21:31 +0300

    feat: Add check for the presence of a filename in the --import command

--- src/core/cli-command/import.command.ts
+++ src/core/cli-command/import.command.ts
@@ -4,19 +4,21 @@ import type { CliCommandInterface } from './cli-command.interface.js';
 export default class ImportCommand implements CliCommandInterface {
   public readonly name = '--import';
   public async execute(filename: string): Promise<void> {
-
+    if (!filename) {
+      console.error('Не указано имя файла для импорта данных');
+      return;
+    }
     const fileReader = new TSVFileReader(filename.trim());
 
     try {
       await fileReader.read();
       console.log(fileReader.toArray());
     } catch (err) {
-
       if (!(err instanceof Error)) {
         throw err;
       }
-
-      console.log(`Не удалось импортировать данные из файла по причине: «${err.message}»`);
+    
+      console.log(`Не удалось импортировать данные из файла по причине: "${err.message}"`);
     }
   }
 }
