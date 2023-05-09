3daed0cb85c5436cc8f73b5ee1ce9dfe67af255c
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-05-09 20:06:48 +0300

    feat: Added a query to retrieve data

--- src/core/cli-command/generate-command.ts
+++ src/core/cli-command/generate-command.ts
@@ -1,3 +1,5 @@
+import got from 'got';
+
 import { MockData } from '../../types/mock-data.js';
 import { CliCommandInterface } from './cli-command.interface.js';
 
@@ -8,5 +10,11 @@ export default class GenerateCommand implements CliCommandInterface {
   public async execute(...parameters:string[]): Promise<void> {
     const [count, filepath, url] = parameters;
     const offerCount = Number.parseInt(count, 10);
+
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      console.log(`Can't fetch data from ${url}.`);
+    }
   }
 }
