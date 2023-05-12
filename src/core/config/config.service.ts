7fc63737a52067508a5cd10c64e32ea337205d94
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-05-12 20:40:26 +0300

    refactor(ConfigService):  improve ConfigService with error handling and logging

--- src/core/config/config.service.ts
+++ src/core/config/config.service.ts
@@ -1,9 +1,21 @@
+import { DotenvParseOutput, config } from 'dotenv';
 import type { ConfigInterface } from './config.interface.js';
+import { LoggerInterface } from '../logger/logger.interface';
 
 export default class ConfigService implements ConfigInterface {
-  constructor(
-    private readonly config: NodeJS.ProcessEnv
-  ) {}
+  private readonly config: NodeJS.ProcessEnv;
+
+  constructor(private readonly logger: LoggerInterface) {
+    const parsedOutput = config();
+
+    if (parsedOutput.error) {
+      throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
+    }
+
+    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    this.logger.info('.env file found and successfully parsed!');
+  }
+
 
   public get(key: string): string | undefined {
     return this.config[key];
