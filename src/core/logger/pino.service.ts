97032a64a7fa4a0bfdcea72209a87227ef1fa667
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-05-12 20:24:41 +0300

    feat: add PinoService

---
+++ src/core/logger/pino.service.ts
@@ -0,0 +1,27 @@
+import { Logger, pino } from 'pino';
+
+import type { LoggerInterface } from './logger.interface.js';
+
+export default class PinoService implements LoggerInterface {
+  private readonly logger: Logger;
+
+  constructor() {
+    this.logger = pino();
+  }
+
+  public debug(message: string, ...args: unknown[]): void {
+    this.logger.debug(message, ...args);
+  }
+
+  public error(message: string, ...args: unknown[]): void {
+    this.logger.error(message, ...args);
+  }
+
+  public info(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+
+  public warn(message: string, ...args: unknown[]): void {
+    this.logger.warn(message, ...args);
+  }
+}
