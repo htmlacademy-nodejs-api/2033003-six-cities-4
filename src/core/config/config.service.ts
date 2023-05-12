fdad476bde2192af6d1ade364cf0915d1c31d233
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-05-12 20:53:14 +0300

    refactor: enhance ConfigService with type-safe interface and schema validation

--- src/core/config/config.service.ts
+++ src/core/config/config.service.ts
@@ -1,9 +1,11 @@
-import { DotenvParseOutput, config } from 'dotenv';
-import type { ConfigInterface } from './config.interface.js';
-import { LoggerInterface } from '../logger/logger.interface';
+import { config } from 'dotenv';
 
-export default class ConfigService implements ConfigInterface {
-  private readonly config: NodeJS.ProcessEnv;
+import type { ConfigInterface } from './config.interface.js';
+import type { LoggerInterface } from '../logger/logger.interface';
+import { RestSchema, configRestSchema } from './rest.schema.js';
+
+export default class ConfigService implements ConfigInterface<RestSchema> {
+  private readonly config: RestSchema;
 
   constructor(private readonly logger: LoggerInterface) {
     const parsedOutput = config();
@@ -12,12 +14,14 @@ export default class ConfigService implements ConfigInterface {
       throw new Error('Can\'t read .env file. Perhaps the file does not exists.');
     }
 
-    this.config = <DotenvParseOutput>parsedOutput.parsed;
+    configRestSchema.load({});
+    configRestSchema.validate({ allowed: 'strict', output: this.logger.info });
+
+    this.config = configRestSchema.getProperties();
     this.logger.info('.env file found and successfully parsed!');
   }
 
-
-  public get(key: string): string | undefined {
+  public get<T extends keyof RestSchema>(key: T): RestSchema[T] {
     return this.config[key];
   }
 }
