c254eead4b7ccf5369c010cbfee9a2ebc69b8e70
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-06-04 19:33:42 +0300

    feat: add offer rdo

---
+++ src/modules/offer/rdo/offer.rdo.ts
@@ -0,0 +1,58 @@
+import { Expose } from 'class-transformer';
+
+import type { CityCoordinates } from '../../../types/city-coordinates.type.js';
+import { RentalType } from '../../../types/rental-type.enum.js';
+import { Amenities } from '../../../types/amenities.enum.js';
+
+export default class OfferRdo {
+  @Expose()
+  public title!: string;
+
+  @Expose()
+  public description!: string;
+
+  @Expose()
+  public publicationDate!: string;
+
+  @Expose()
+  public city!: string;
+
+  @Expose()
+  public coordinates!: CityCoordinates;
+
+  @Expose()
+  public previewImage!: string;
+
+  @Expose()
+  public images!: string[];
+
+  @Expose()
+  public isPremium!: boolean;
+
+  @Expose()
+  public isFavorite!: boolean;
+
+  @Expose()
+  public rating!: number;
+
+  @Expose()
+  public type!: RentalType;
+
+  @Expose()
+  public rooms!: number;
+
+  @Expose()
+  public guests!: number;
+
+  @Expose()
+  public price!: number;
+
+  @Expose()
+  public amenities!: Amenities[];
+
+  @Expose()
+  public authorId!: string;
+
+  @Expose()
+  public commentCount!: number;
+}
