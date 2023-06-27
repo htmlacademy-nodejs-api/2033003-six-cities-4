5b5ffc6f5ef1f69ccb644a3134e45cac6f487cff
ALEKSANDR FELYUGIN <feliugin@gmail.com>
2023-06-27 09:46:55 +0300

    feat: add description

--- src/core/cli-command/helper.command.ts
+++ src/core/cli-command/helper.command.ts
@@ -11,9 +11,9 @@ export default class HelpCommand implements CliCommandInterface {
         Пример:
             main.js --<command> [--arguments]
         Команды:
-            ${chalk.green('--version')}:                   # Выводит номер версии приложения
-            ${chalk.green('--help')}:                      # Печатает этот текст
-            ${chalk.green('--import <path>')}:             # Импортирует в базу данных информацию из tsv-файла. Путь к файлу передаётся в параметре <path>.
+            ${chalk.green('--version')}: # Выводит номер версии приложения
+            ${chalk.green('--help')}:  # Печатает этот текст
+            ${chalk.green('--import <path> <login> <password> <host> <dbname> <salt>')}: # Импортирует в базу данных информацию из tsv-файла. Путь к файлу передаётся в параметре <path>.
             ${chalk.green('--generate <n> <path> <url>')}:  # Создаёт файл в формате tsv с тестовыми данными. Параметр <n> задаёт количество генерируемых предложений. Параметр <path> указывает путь для сохранения файла с предложениями. Параметр <url> задаёт адрес сервера, с которого необходимо взять данные
     `));
   }
